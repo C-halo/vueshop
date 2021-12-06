ea14747b85b2cd6b1e2c02da1cca92619b54ea0f
Halo <halo@123.com>
2021-12-06 21:51:09 +0800

    登录功能

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入axios包
+import axios from 'axios'
 import './plugins/element.js'
 
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 将axios挂载到Vue原型对象上
+Vue.prototype.$http = axios
 
 new Vue({
   router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, submenu, MenuItem, MenuItemGroup, Breadcrumb, BreadcrumbItem, Card, Col, Row, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane, Steps, Step, CheckboxGroup, Checkbox, Upload } from 'element-ui'
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
